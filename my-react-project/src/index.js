0713b25ad6a19faff6783c6d621f79061a95b956
Subesh-ux <subeshsubi7305@gmail.com>
2026-08-13 20:37:21 +0530

    task is completed

--- my-react-project/src/index.js
+++ my-react-project/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import StudentDetails from './pages/StudentDetails';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Welcome to React JS</h1>
-  </React.StrictMode>
+    < StudentDetails />
 );
 
 
